b3922b4fff0e4983a2728b77c8be273c4d0df10d
Ana Aparicio <aaparici@student.42madrid.com>
2022-07-18 22:18:01 +0200

    Storage Factory

--- SotorageFactory.sol
+++ SotorageFactory.sol
@@ -1,10 +1,26 @@
-/SPDX-License-Identifier: MIT
+//SPDX-License-Identifier: MIT
 
 pragma solidity >=0.6.0 <0.9.0;
 
-import "./SimpleStorage.sol"
+import "./SimpleStorage.sol";
 
 contract StorageFactory 
 {
-    
+    SimpleStorage[] public StoragesArray;
+
+    function CreateStorageContract() public
+    {
+        SimpleStorage UnicContract = new SimpleStorage();
+        StoragesArray.push(UnicContract);
+    }
+
+    function sfStorage(uint256 _Index, int256 _simpleStorageNumber) public
+    {
+        StoragesArray[_Index].store(_simpleStorageNumber);
+    }
+
+    function sfRetrieve(uint256 _Index) public view returns (int256)
+    {
+        return StoragesArray[_Index].retrieve();
+    }
 }
